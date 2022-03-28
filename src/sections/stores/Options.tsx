65beb6654f70cb80553970b195b49f1035afff56
Max Edwards <max@solotodo.com>
2022-03-28 17:44:34 -0300

    add options links

--- src/sections/stores/Options.tsx
+++ src/sections/stores/Options.tsx
@@ -1,39 +1,83 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Card, CardHeader } from "@mui/material";
-import { Store } from "src/frontend-utils/types/store";
+import NextLink from "next/link";
+import {
+  Card,
+  CardHeader,
+  List,
+  ListItem,
+  ListItemText,
+  Link,
+} from "@mui/material";
+import { PATH_STORE } from "src/routes/paths";
+import { useRouter } from "next/router";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
+type Option = {
+  key: number;
+  text: string;
+  path: string;
+};
 
 export default function CustomizedTables() {
+  const router = useRouter();
+  const baseRoute = `${PATH_STORE.root}/${router.query.id}`
+  const options: Option[] = [
+    {
+      key: 1,
+      text: "Información general",
+      path: baseRoute,
+    },
+    {
+      key: 2,
+      text: "Actualizar pricing",
+      path: `${baseRoute}/update_pricing`,
+    },
+    {
+      key: 3,
+      text: "Registros de actualización",
+      path: `${baseRoute}/update_logs`,
+    },
+    {
+      key: 4,
+      text: "Leads (listado)",
+      path: `${baseRoute}`,
+    },
+    {
+      key: 5,
+      text: "Leads (estadísticas)",
+      path: `${baseRoute}`,
+    },
+    {
+      key: 6,
+      text: "Entidades en conflicto",
+      path: `${baseRoute}`,
+    },
+    {
+      key: 7,
+      text: "Ratings",
+      path: `${baseRoute}`,
+    },
+    {
+      key: 8,
+      text: "Descargar reporte de homologación",
+      path: `${baseRoute}`,
+    },
+  ];
   return (
     <Card style={{ padding: 10 }}>
       <CardHeader title="Opciones" sx={{ mb: 3 }} />
-      
+      <List>
+        {options.map((o) => (
+          <ListItem key={o.key}>
+            <ListItemText
+              primary={
+                <NextLink href={o.path} passHref>
+                  <Link>{o.text}</Link>
+                </NextLink>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
 }
