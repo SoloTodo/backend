a5670a346941138ace52d5688f45f17b74da462a
Max Edwards <max@solotodo.com>
2022-05-13 14:35:43 -0400

    new reports

---
+++ src/pages/reports/wtb_report.tsx
@@ -0,0 +1,159 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Grid,
+} from "@mui/material";
+import { GetServerSideProps } from "next/types";
+import { useSnackbar } from "notistack";
+import { ReactElement } from "react";
+import HeaderBreadcrumbs from "src/components/HeaderBreadcrumbs";
+import Page from "src/components/Page";
+import ApiFormComponent from "src/frontend-utils/api_form/ApiFormComponent";
+import ApiFormSelectComponent from "src/frontend-utils/api_form/fields/select/ApiFormSelectComponent";
+import ApiFormSubmitComponent from "src/frontend-utils/api_form/fields/submit/ApiFormSubmitComponent";
+import { jwtFetch } from "src/frontend-utils/nextjs/utils";
+import {
+  selectApiResourceObjects,
+  useApiResourceObjects,
+} from "src/frontend-utils/redux/api_resources/apiResources";
+import { apiSettings } from "src/frontend-utils/settings";
+import { Brand } from "src/frontend-utils/types/wtb";
+import Layout from "src/layouts";
+import { PATH_DASHBOARD, PATH_REPORTS } from "src/routes/paths";
+import { useAppSelector } from "src/store/hooks";
+
+// ----------------------------------------------------------------------
+
+ReportWtb.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+// ----------------------------------------------------------------------
+
+export default function ReportWtb({ brands }: { brands: Brand[] }) {
+  const apiResourceObjects = useAppSelector(useApiResourceObjects);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const brandChoices = brands.map((b) => ({ label: b.name, value: b.id }));
+
+  const fieldsMetadata = [
+    {
+      fieldType: "select" as "select",
+      name: "brands",
+      label: "Marcas",
+      multiple: false,
+      choices: brandChoices,
+    },
+    {
+      fieldType: "select" as "select",
+      name: "categories",
+      label: "Categorías",
+      multiple: true,
+      choices: selectApiResourceObjects(apiResourceObjects, "categories"),
+    },
+    {
+      fieldType: "select" as "select",
+      name: "currency",
+      label: "Moneda",
+      multiple: false,
+      choices: selectApiResourceObjects(apiResourceObjects, "currencies"),
+    },
+    {
+      fieldType: "select" as "select",
+      name: "stores",
+      label: "Tiendas",
+      multiple: true,
+      choices: selectApiResourceObjects(apiResourceObjects, "stores"),
+    },
+    {
+      fieldType: "select" as "select",
+      name: "countries",
+      label: "Países",
+      multiple: true,
+      choices: selectApiResourceObjects(apiResourceObjects, "countries"),
+    },
+    {
+      fieldType: "select" as "select",
+      name: "store_types",
+      label: "Tipos",
+      multiple: true,
+      choices: selectApiResourceObjects(apiResourceObjects, "types"),
+    },
+    {
+      fieldType: "submit" as "submit",
+      name: "submit",
+    },
+  ];
+
+  return (
+    <Page title="WTB | Reportes">
+      <Container maxWidth={false}>
+        <HeaderBreadcrumbs
+          heading=""
+          links={[
+            { name: "Inicio", href: PATH_DASHBOARD.root },
+            { name: "Reportes", href: PATH_REPORTS.root },
+            { name: "WTB" },
+          ]}
+        />
+        <ApiFormComponent
+          fieldsMetadata={fieldsMetadata}
+          endpoint={`${apiSettings.apiResourceEndpoints.reports}current_prices/`}
+          requiresSubmit={true}
+          onResultsChange={() =>
+            enqueueSnackbar(
+              "El reporte está siendo generado. Una vez finalizado este será enviado a su correo"
+            )
+          }
+        >
+          <Card>
+            <CardHeader title="Filtros" />
+            <CardContent>
+              <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+                columns={{ xs: 6, md: 12 }}
+              >
+                <Grid item xs={6}>
+                  <ApiFormSelectComponent name="brands" />
+                </Grid>
+                <Grid item xs={6}>
+                  <ApiFormSelectComponent name="categories" />
+                </Grid>
+                <Grid item xs={6}>
+                  <ApiFormSelectComponent name="currency" />
+                </Grid>
+                <Grid item xs={6}>
+                  <ApiFormSelectComponent name="stores" />
+                </Grid>
+                <Grid item xs={6}>
+                  <ApiFormSelectComponent name="countries" />
+                </Grid>
+                <Grid item xs={6}>
+                  <ApiFormSelectComponent name="store_types" />
+                </Grid>
+                <Grid item xs={6}>
+                  <ApiFormSubmitComponent name="submit" />
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </ApiFormComponent>
+      </Container>
+    </Page>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const brands = await jwtFetch(
+    context,
+    apiSettings.apiResourceEndpoints.wtb_brands
+  );
+  return {
+    props: {
+      brands: brands,
+    },
+  };
+};
