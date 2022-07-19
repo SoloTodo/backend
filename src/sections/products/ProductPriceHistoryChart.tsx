dab89fa4b769491844d4526ee99e29fcc9cc5e77
Max Edwards <max@solotodo.com>
2022-07-19 11:00:11 -0400

    fix x-axis por different entities

--- src/sections/products/ProductPriceHistoryChart.tsx
+++ src/sections/products/ProductPriceHistoryChart.tsx
@@ -1,16 +1,20 @@
+import currency from "currency.js";
+import { addDays } from "date-fns";
 import merge from "lodash/merge";
 import { useContext } from "react";
 import ApiFormContext from "src/frontend-utils/api_form/ApiFormContext";
+import { ApiFormDateRangePicker } from "src/frontend-utils/api_form/fields/range_picker/ApiFormDateRangePicker";
 import { ApiFormSelect } from "src/frontend-utils/api_form/fields/select/ApiFormSelect";
 import { useApiResourceObjects } from "src/frontend-utils/redux/api_resources/apiResources";
 import { Entity } from "src/frontend-utils/types/entity";
 import { useAppSelector } from "src/store/hooks";
+import { fDate } from "src/utils/formatTime";
 // components
 import ReactApexChart, { BaseOptionChart } from "../../components/chart";
 
 // ----------------------------------------------------------------------
 
-export default function ProductPriceHistoryChart({name}: {name:string}) {
+export default function ProductPriceHistoryChart({ name }: { name: string }) {
   const apiResourceObjects = useAppSelector(useApiResourceObjects);
   const context = useContext(ApiFormContext);
   let currentResult = context.currentResult;
@@ -21,21 +25,105 @@ export default function ProductPriceHistoryChart({name}: {name:string}) {
     throw `Invalid field name: ${name}`;
   }
 
-  let useOfferPrice = false
-  if (typeof field.cleanedData !== "undefined" && field.cleanedData.length !== 0) {
-    useOfferPrice = field.cleanedData[0].value === 'offer'
+  let useOfferPrice = false;
+  if (
+    typeof field.cleanedData !== "undefined" &&
+    field.cleanedData.length !== 0
+  ) {
+    useOfferPrice = field.cleanedData[0].value === "offer";
+  }
+
+  const fieldTimestamp = context.getField("timestamp") as
+    | ApiFormDateRangePicker
+    | undefined;
+
+  if (typeof fieldTimestamp === "undefined") {
+    throw `Invalid field name: timestamp`;
+  }
+
+  const cleanedData =
+    typeof fieldTimestamp.cleanedData !== "undefined"
+      ? fieldTimestamp.cleanedData
+      : [null, null];
+
+  let days: Date[] = [];
+  if (cleanedData[0] !== null && cleanedData[1] !== null) {
+    var day = cleanedData[0];
+    day.setHours(0, 0, 0);
+    while (day <= cleanedData[1]) {
+      days.push(day);
+      day = addDays(day, 1);
+    }
+  }
+
+  const finalData = [];
+
+  for (const pricingEntry of currentResult as {
+    entity: Entity;
+    pricing_history: any[];
+  }[]) {
+    if (pricingEntry.entity.condition != "https://schema.org/NewCondition")
+      continue;
+
+    const newData = {
+      entity: pricingEntry.entity,
+      normalized_pricing_history: {
+        normalPrices: {} as Record<
+          string,
+          { timestamp: string; normalPrice: currency }
+        >,
+        offerPrices: {} as Record<
+          string,
+          { timestamp: string; offerPrice: currency }
+        >,
+      },
+    };
+
+    for (const priceHistory of pricingEntry.pricing_history) {
+      if (!priceHistory.is_available) continue;
+
+      const timestamp = fDate(new Date(priceHistory.timestamp));
+      const normalPrice = currency(priceHistory.normal_price, {
+        precision: 0,
+      });
+      const offerPrice = currency(priceHistory.offer_price, {
+        precision: 0,
+      });
+
+      newData.normalized_pricing_history.normalPrices[timestamp] = {
+        timestamp,
+        normalPrice,
+      };
+      newData.normalized_pricing_history.offerPrices[timestamp] = {
+        timestamp,
+        offerPrice,
+      };
+    }
+
+    finalData.push(newData);
   }
 
   const CHART_DATA: any = [];
 
-  currentResult.map(({entity, pricing_history}: {entity: Entity, pricing_history: any[]}) =>
-    CHART_DATA.push(
-      {
-        name: apiResourceObjects[entity.store].name,
-        data: pricing_history.map((p) => useOfferPrice ? p.offer_price : p.normal_price),
-        type: "line",
-      }
-    )
+  finalData.map((d) =>
+    CHART_DATA.push({
+      name: apiResourceObjects[d.entity.store].name,
+      data: days.map((day) => {
+        const pricing = useOfferPrice
+          ? d.normalized_pricing_history.offerPrices[fDate(day)]
+          : d.normalized_pricing_history.normalPrices[fDate(day)];
+        if (pricing) {
+          return useOfferPrice
+            ? (pricing as { timestamp: string; offerPrice: currency })
+                .offerPrice.value
+            : (pricing as { timestamp: string; normalPrice: currency })
+                .normalPrice.value;
+        } else {
+          return null;
+        }
+      }),
+      type: "line",
+    })
   );
 
   const chartOptions = merge(BaseOptionChart(), {
@@ -44,17 +132,24 @@ export default function ProductPriceHistoryChart({name}: {name:string}) {
     },
     xaxis: {
       type: "datetime",
-      categories: currentResult.length !== 0 ? currentResult[0].pricing_history.map((p: { timestamp: string }) => p.timestamp) : [],
+      categories: days.map((d) => d.toISOString()),
     },
-    tooltip: { x: { format: "dd/MM/yy" }, marker: { show: false } },
+    yaxis: {
+      title: {
+        text: "Precio",
+      },
+      labels: {
+        formatter: (value: number) =>
+          currency(value, { precision: 0 }).format(),
+      },
+    },
+    tooltip: {
+      shared: false,
+      intersect: true,
+    },
   });
 
   return (
-    <ReactApexChart
-      type="line"
-      series={CHART_DATA}
-      options={chartOptions}
-      // height={400}
-    />
+    <ReactApexChart type="line" series={CHART_DATA} options={chartOptions} />
   );
 }
