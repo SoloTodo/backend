8bb0698d6562bc42fd52c1cdb9a834c738dce3d6
Max Edwards <max@solotodo.com>
2022-12-26 13:16:20 -0300

    add pagination table

--- src/pages/metamodel/models/[id]/index.tsx
+++ src/pages/metamodel/models/[id]/index.tsx
@@ -1,30 +1,26 @@
 import {
-  Box,
   Button,
   Card,
   CardContent,
-  CircularProgress,
   Container,
   Link,
   Stack,
 } from "@mui/material";
 import { GridColDef } from "@mui/x-data-grid";
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import NextLink from "next/link";
 import HeaderBreadcrumbs from "src/components/HeaderBreadcrumbs";
 import Page from "src/components/Page";
 import Layout from "src/layouts";
 import { PATH_DASHBOARD, PATH_METAMODEL } from "src/routes/paths";
-import BasicTable from "src/sections/BasicTable";
 import { jwtFetch } from "src/frontend-utils/nextjs/utils";
 import { apiSettings } from "src/frontend-utils/settings";
-import {
-  InstanceMetaModel,
-  MetaModel,
-} from "src/frontend-utils/types/metamodel";
+import { MetaModel } from "src/frontend-utils/types/metamodel";
 import { GetServerSideProps } from "next/types";
 import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
+import ApiFormPaginationTable from "src/components/api_form/ApiFormPaginationTable";
+import ApiFormComponent from "src/frontend-utils/api_form/ApiFormComponent";
 
 // ----------------------------------------------------------------------
 
@@ -39,43 +35,26 @@ export default function MetaModelinstanceList({
 }: {
   metaModel: MetaModel;
 }) {
-  const [isLoading, setLoading] = useState(false);
-  const [modelInstances, setModelInstances] = useState<InstanceMetaModel[]>([]);
-
-  useEffect(() => {
-    const myAbortController = new AbortController();
-
-    setLoading(true);
-    jwtFetch(
-      null,
-      `${apiSettings.apiResourceEndpoints.metamodel_instance_models}?models=${metaModel.id}`,
-      {
-        signal: myAbortController.signal,
-      }
-    )
-      .then((res) => {
-        setModelInstances(res);
-        setLoading(false);
-      })
-      .catch((_) => {});
-    return () => {
-      myAbortController.abort();
-    };
-  }, []);
+  const fieldMetadata = [
+    {
+      fieldType: "pagination" as "pagination",
+    },
+    {
+      fieldType: "text" as "text",
+      name: "search",
+    },
+  ];
 
   const columns: GridColDef[] = [
     {
       headerName: "Instancias",
       field: "unicode_representation",
       flex: 1,
-      renderCell: (params) => (
-        <NextLink
-          href={`${PATH_METAMODEL.instances}/${params.row.id}`}
-          passHref
-        >
+      renderCell: (row: any) => (
+        <NextLink href={`${PATH_METAMODEL.instances}/${row.id}`} passHref>
           <Link>
-            {params.row.unicode_representation
-              ? params.row.unicode_representation
+            {row.unicode_representation
+              ? row.unicode_representation
               : "[Sin Unicode Representation]"}
           </Link>
         </NextLink>
@@ -94,12 +73,11 @@ export default function MetaModelinstanceList({
             { name: metaModel.name },
           ]}
         />
-        {isLoading ? (
-          <Box textAlign="center">
-            <CircularProgress color="inherit" />
-          </Box>
-        ) : (
-          <Stack spacing={3}>
+        <Stack spacing={3}>
+          <ApiFormComponent
+            fieldsMetadata={fieldMetadata}
+            endpoint={`${apiSettings.apiResourceEndpoints.metamodel_instance_models}?models=${metaModel.id}`}
+          >
             <Card>
               <CardContent>
                 <Stack spacing={2} direction="row">
@@ -126,13 +104,13 @@ export default function MetaModelinstanceList({
                 </Stack>
               </CardContent>
             </Card>
-            <BasicTable
-              title={metaModel.name}
+            <ApiFormPaginationTable
               columns={columns}
-              data={modelInstances}
+              title={metaModel.name}
+              withSearch
             />
-          </Stack>
-        )}
+          </ApiFormComponent>
+        </Stack>
       </Container>
     </Page>
   );
