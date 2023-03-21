8ba689aa25ced0cc2ca1de97afe24c234fd3faff
Max Edwards <max@solotodo.com>
2023-03-21 12:44:52 -0300

    add download method

--- src/sections/banners/BannerActiveParticipactionReportButton.tsx
+++ src/sections/banners/BannerActiveParticipactionReportButton.tsx
@@ -1,16 +1,21 @@
 import { Button } from "@mui/material";
 import { useContext, useState } from "react";
 import ApiFormContext from "src/frontend-utils/api_form/ApiFormContext";
+import { jwtFetch } from "src/frontend-utils/nextjs/utils";
 
 export default function BannerActiveParticipactionReportButton() {
   const [loading, setLoading] = useState(false);
   const context = useContext(ApiFormContext);
 
-  console.log(context);
-
   const handleReportClick = () => {
     setLoading(true);
+    const queryUrl = context.getQueryUrl();
+    jwtFetch(null, `${queryUrl.href}&response_format=xls`).then((json) => {
+      window.location = json.url;
+      setLoading(false);
+    });
   };
+
   return (
     <Button variant="contained" disabled={loading} onClick={handleReportClick}>
       {loading ? "Generando..." : "Descargar"}
